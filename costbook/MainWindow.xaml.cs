e49bcaebd40d89a4b5ebeb7f13d0691fcdc380c6
LAPTOP-ATODRQ4Q\YISHAN <egg880604@gmail.com>
2018-06-21 02:23:44 +0800

    I do my best

---
+++ costbook/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace costbook
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void total()
+        {
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 產生一個方塊
+            taskbox item = new taskbox();
+
+            // 放到 Todostack
+            TodoStack.Children.Add(item);
+        }
+
+        private void SaveBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // 產生一個方塊
+            taskbox item = new taskbox();
+
+            // 放到 Todostack
+            TodoStack.Children.Add(item);
+        }
+
+        private void taskbox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+
+        private void DeleteItem(object sender, EventArgs e)
+        {
+            DODO.Children.Remove((taskbox)sender);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            List<string> datas = new List<string>();
+
+            foreach (taskbox item in DODO.Children)
+            {
+                string line = "";
+                line += "|" + item.date + "|" + item.th + "|" + item.cost;
+                datas.Add(line);
+
+            }
+
+            System.IO.File.WriteAllLines(@"D:\temp\data.txt", datas);
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\temp\data.txt");
+
+    foreach (string line in lines)
+            {
+
+                string[] parts = line.Split('|');
+
+                taskbox item = new taskbox();
+                item.TH = parts[1];
+                item.DeleteItem += new EventHandler(DeleteItem);
+ DODO.Children.Add(item);
+            }
+        }
+    }
+}
+
---
+++ costbook/taskbox.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace costbook
+{
+    /// <summary>
+    /// taskbox.xaml 的互動邏輯
+    /// </summary>
+    public partial class taskbox : UserControl
+
+    {
+
+        
+
+        private void datebox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void thing_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void money_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        public string Dates
+        {
+            get
+            {
+                return date.Text;
+            }
+            set
+            {
+                date.Text = value;
+            }
+
+        }
+        public string TH
+        {
+            get
+            {
+                return th.Text;
+            }
+            set
+            {
+                th.Text = value;
+            }
+
+        }
+        public string ItemPrice
+        {
+            get
+            {
+                return cost.Text;
+            }
+            set
+            {
+                cost.Text = value;
+            }
+
+        }
+        public taskbox()
+        {
+            InitializeComponent();
+        }
+        // 自訂刪除事件
+        public event EventHandler DeleteItem;
+
+        // 項目名稱鍵盤按下事件
+        private void TaskNamePreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // 任務空白，而且按下 Backspace 鍵時，引發 DeleteItem 事件
+            if (th.Text == "" && e.Key == Key.Back)
+            {
+                DeleteItem(this, null);
+            }
+        }
+        public int itemPriceValue
+        {
+            get
+            {
+                //嘗試解析價格
+                try
+                {
+                    return int.Parse(cost.Text);
+                }
+                //失敗後要求用家輸入數字
+                catch
+                {
+                    MessageBox.Show("請輸入數字");
+                    return 0;
+                }
+            }
+            set
+            {
+                cost.Text = value.ToString();
+            }
+        }
+
+
+    }
+
+}
+    
